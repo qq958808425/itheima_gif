49293a21267d8f7dc33551bdc6c8154504d3039d
justin_zhang <958808425@qq.com>
2021-01-05 20:42:53 +0800

    项目初始化

---
+++ day49_gif/src/main/java/com/itheima/pojo/User.java
@@ -0,0 +1,13 @@
+package com.itheima.pojo;
+
+/**
+ * 包名:com.itheima.pojo
+ *
+ * @author Zhang Baoxian
+ * 日期:2021-01-05   20:07:55
+ */
+public class User {
+    private String username;
+    private Integer password;
+    private String nickname;
+}
